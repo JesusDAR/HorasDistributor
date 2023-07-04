f4d8126041d835fb60a9679b82c8a1642bfbee5c
j.dar <j.dearagon.2016@alumnos.urjc.es>
2023-07-04 19:57:51 +0200

    Added WorkPackage Model

---
+++ HorasDistributor.Data/Models/WorkPackage.cs
@@ -0,0 +1,9 @@
+﻿namespace HorasDistributor.Data.Models
+{
+    public class WorkPackage
+    {
+        public string Nombre { get; set; }
+        public float Horas { get; set; }
+        public float Coste { get; set; }
+    }
+}
