7d28b00cac2feefda2a581b33d0a1873640f4306
j.dar <j.dearagon.2016@alumnos.urjc.es>
2023-07-04 19:57:18 +0200

    Added Investigador Model

---
+++ HorasDistributor.Data/Models/Investigador.cs
@@ -0,0 +1,9 @@
+﻿namespace HorasDistributor.Data.Models
+{
+    public class Investigador
+    {
+        public string Nombre { get; set; }
+        public float Horas { get; set; }
+        public float CosteHora { get; set; }
+    }
+}
